347637a0177b5d59803f6c76dd402070ba1baa03
JeongWonLim <cdnljw@gmail.com>
2022-06-11 15:31:11 +0900

    test 추가

--- chartData.py
+++ chartData.py
@@ -1,4 +1,3 @@
-from multiprocessing.sharedctypes import synchronized
 from pykrx import stock
 from pykrx import bond
 import requests
@@ -13,36 +12,7 @@ from pykrx import stock
 import pandas as pd
 import numpy as np
 import sqlalchemy as db
-
-
-class datesave:
-    def __init__(self):
-        engine = db.create_engine('mysql+pymysql://root:root@localhost/a')
-        pass
-    
-    def get_market_ohlcv(self, now=datetime.now()):
-        df = stock.get_market_ohlcv(now.replace('-',''), market='ALL')
-        df.columns = ['code', 'open', 'high', 'low', 'close', 'volume', 'value', 'rate', 'marketcap', 'shares']
-        df['date'] = now
-        return df
-    
-    def df_save(self, df, user,password, server, db, table):
-        engine = create_engine('mysql+pymysql://{}:{}@{}/{}?charset=utf8'.format(user, password, server, db))
-        df.to_sql(name=table, con=engine, if_exists='append', index=False)
-        self.engine.dispose()
-    
-    def date_range(self, start, end):
-        start = datetime.strptime(start, "%Y-%m-%d")
-        end = datetime.strptime(end, "%Y-%m-%d")
-        dates = [(start + timedelta(days=i)).strftime("%Y-%m-%d") for i in range((end-start).days+1)]
-        return dates
-        
-    def df_replace_save(self, df : pd.DataFrame, user, password, server, db, table):
-        for idx in range(len(df)):
-            columns = ','.join(df.columns)
-            values = ["'"+str(x)+"'" for x in df.iloc[idx]]
-            sql = 'replace into {2}({0}) values({1})'.format(columns, ','.join(values), table)            
-            self.engine.execute(sql)
+from db import data 
 
 OPEN = 'OPEN_PRICE'
 CLOSE = 'CLOSE_PRICE'
@@ -94,47 +64,20 @@ def get_chartdata_days(start:str, end : str, stkCode):
     
     return df.DataFrame()
         
-        
+def get_company_info(): 
+    
+
 if __name__ =='__main__':
+    save = data('root','root', 'localhost', 'my_stock')
     code = stock.get_market_ticker_list(datetime.now().strftime('%Y-%m-%d'), 'KOSPI')
     time.sleep(4)
     code += stock.get_market_ticker_list(datetime.now().strftime('%Y-%m-%d'), 'KOSDAQ')
-    # saved = code.copy()
-    # a = datesave()
-    # cnt= 0
-    # # for d in code:
-    # #     cnt +=1
-    # #     print(cnt)
-    # #     df = get_chartdata_days('20170101','20220511', d)
-    # #     print(df)
-    # #     try:
-    # #         a.df_save(df, 'root', 'root', 'localhost', 'stock', 'stock')
-    # #         saved.pop(saved.index(d))
-    # #     except :
-    # #         print('에러')
-    # #         print(saved)
-    # #         pass
-    # #     time.sleep(15)
 
-    # from FnGuide import fnguide
-    
-    # Fn = fnguide()
-    # for d in code:
-    #     print(d)
-    #     cnt +=1
-    #     print(cnt)
-
-    #     try:
-    #         df = Fn.get_Finance(d, 'D')           
-    #         print('에러확인')
-    #         a.df_save(df[0], 'root', 'root', 'localhost', 'stock', 'sonik')
-    #         a.df_save(df[1], 'root', 'root', 'localhost', 'stock', 'deacha')
-    #         a.df_save(df[2], 'root', 'root', 'localhost', 'stock', 'cash')
-    #     except :
-    #         print('에러')
-    #         pass
-    #     time.sleep(10)
-        
-    t = datesave()
-    t.df_replace_save(df, 'root', 'root', 'localhost', 'a', 'b')
-    
+    for d in code:
+        df = get_chartdata_days('20170101','20220511', d)
+        try:
+            save.df_replace_save(df, 'ohlcv')
+        except :
+            print("에러")
+            pass
+        time.sleep(15)
--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 from JIpyo import *
 
-df = getCompanyInfo('2022-05-10')
+#22년 5월 10일자에 투자한다고 가정했을 시 
+#5월 10일날 주가, 재무제표를 가져옴
+df = getCompanyInfo('2022-05-10') 
 
 #4 대장 콤보
 df=getPER(df)
 df=getPBR(df)
 df=getPSR(df)
 df=getPCR(df)
-s1 = df_filter(df, 'PER', min= 1, n=400, asc = True) #하위 50종목
-s2 = df_filter(df, 'PBR', min=0.1,  n=400, asc= True) # 하위 50종목
-s3 = df_filter(df, 'PSR', min=0.1,  n=400, asc= True) # 하위 50종목
-s4 = df_filter(df, 'PCR', min=0.1, n=400, asc= True) # 하위 50종목
+s1 = df_filter(df, 'PER', min= 1, n=400, asc = True) #하위 400종목
+s2 = df_filter(df, 'PBR', min=0.1,  n=400, asc= True) # 하위 400종목
+s3 = df_filter(df, 'PSR', min=0.1,  n=400, asc= True) # 하위 400종목
+s4 = df_filter(df, 'PCR', min=0.1, n=400, asc= True) # 하위 400종목
 #df_combine(s1,s2,s3,s4 , how='and').to_csv("./4대장콤보.csv",index = False)
 
 #EV/EBITDA
@@ -33,7 +35,7 @@ df = getROE(df)
 s1 = df_filter(df, 'ROE', min = 0, n = 30, asc=False)
 s1.to_csv("./ROE.csv", index=False)
 
-#잔여이익 모델 저평가 상태
+#PRIM잔여이익 모델 저평가 상태
 df = getRIM(df, 0.1)
 df['P_RIM'] = df['CLOSE_PRICE'] / df['RIM']
 s1 = df_filter(df, 'P_RIM', min = 0, max=1, n =30, asc=True)
@@ -49,8 +51,15 @@ df = getLiabRatio(df)
 s1 = df_filter(df, 'LiabRatio', min = 0, n = 30, asc=True)
 s1.to_csv('./부채비율.csv', index=False)
 
-#성장률 지표
-df = get
+#매출액 증가율이 높은 회사
+df = getRG(df, 2)
+s1 = df_filter(df, 'RG', min = 0, n = 30, asc=False)
+s1.to_csv('./매출액 증가율높은 회사 투자.csv', index = False)
+
+#영업이익 증가율이 높은 회사
+df = getOIG(df, 2)
+s1 = df_filter(df, 'OIG', min = 0, n = 30, asc=False)
+s1.to_csv('영업이익 증가율이 높은회사 투자.csv', index = False)
 
 #그린블라트 마법공식
 df = getPER(df)
